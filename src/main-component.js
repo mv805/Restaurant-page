c9782a042764e0f8e7e9f69c3ae1dbc03476ccab
Matt Villa <matthewvilla805@gmail.com>
2022-06-05 00:50:44 -0700

    Initial Commit

---
+++ src/main-component.js
@@ -0,0 +1,9 @@
+
+//build base format --index
+//      build-header --index
+//      build footer --index
+//build main page --main
+//      splash image --main
+//      about us --main
+//      services --main
+//cleanup/remove components --index
